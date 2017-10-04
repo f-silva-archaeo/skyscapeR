12ed2d2ebb7188e54b85cb56136cf69a5ff2c0d7
f-silva-archaeo <fabio.silva@ucl.ac.uk>
2017-10-04 17:14:09 +0200

    v.0.1.6 Change log:  - fixed issue with palaeo.star() not detecting astrolibR functions  - added resolution as a parameter to orbit()  - added object() for easy target selection for plotting  - added objects as parameters to all plotting functions  - changed orplot to plotAz  - manual pages udpated throughout

--- R/mechanics.R
+++ R/mechanics.R
@@ -22,6 +22,8 @@ cur.year <- as.numeric(format(Sys.Date(), "%Y")) # current year
 #' #' # Obliquity for year 3999 BC:
 #' obliquity(-4000)
 obliquity = function(year = cur.year) {
+  require(palinsol)
+
   aux <- palinsol::la04(year-1950, degree=T)
   names(aux) <- NULL
   return(aux[1])
@@ -144,7 +146,7 @@ sMjLX = function(year = cur.year) {
 #' @seealso \code{\link{jS}}, \code{\link{dS}}, \code{\link{zenith}}, \code{\link{antizenith}}
 #' @examples
 #' eq()
-eq = function() {
+eq = function(bh=NULL) {
   return(0)
 }
 
@@ -155,8 +157,8 @@ eq = function() {
 #'  this phenomena does not occur at given location
 #'  (i.e. if location is outside the tropical band)
 #'  the function returns a NULL value.
-#' @param loc This can be either the latitude of the l
-#' ocation, or a skyscapeR.horizon object
+#' @param loc This can be either the latitude of the
+#' location, or a skyscapeR.horizon object.
 #' @export
 #' @seealso \code{\link{jS}}, \code{\link{dS}}, \code{\link{eq}}, \code{\link{antizenith}}
 #' @examples
@@ -222,24 +224,24 @@ antizenith = function(loc) {
 #' Sirius <- star('Sirius')
 #' Sirius.2500BC <- palaeo.star(Sirius, -2501)
 palaeo.star = function(star, year = cur.year) {
-  if (class(star)=="star") {
+  if (class(star)=="skyscapeR.star") {
     # precession
     trash <- capture.output(coor <- astrolibR::precess(star$ra,star$dec, 2000, year), file=NULL)
 
     # proper motion
     adj.pm <- star$proper.motion*(year-2000)/1000
-    coor$ra <- coor$ra + ten(0,0,adj.pm[1])*sign(adj.pm[1])
-    coor$dec <- coor$dec + ten(0,0,adj.pm[2])*sign(adj.pm[2])
+    coor$ra <- coor$ra + astrolibR::ten(0,0,adj.pm[1])*sign(adj.pm[1])
+    coor$dec <- coor$dec + astrolibR::ten(0,0,adj.pm[2])*sign(adj.pm[2])
 
     # nutation
-    adj.nu <- astrolibR::co_nutate(jdcnv(year,1,1,0), star$ra, star$dec)
-    coor$ra <- coor$ra + ten(0,0,adj.nu$d_ra)*sign(adj.nu$d_ra)
-    coor$dec <- coor$dec + ten(0,0,adj.nu$d_dec)*sign(adj.nu$d_dec)
+    adj.nu <- astrolibR::co_nutate(astrolibR::jdcnv(year,1,1,0), star$ra, star$dec)
+    coor$ra <- coor$ra + astrolibR::ten(0,0,adj.nu$d_ra)*sign(adj.nu$d_ra)
+    coor$dec <- coor$dec + astrolibR::ten(0,0,adj.nu$d_dec)*sign(adj.nu$d_dec)
 
     # aberration
-    adj.ab <- astrolibR::co_aberration(jdcnv(year,1,1,0), star$ra, star$dec, deg2rad(obliquity(year)))
-    coor$ra <- coor$ra + ten(0,0,adj.ab$d_ra)*sign(adj.ab$d_ra)
-    coor$dec <- coor$dec + ten(0,0,adj.ab$d_dec)*sign(adj.ab$d_dec)
+    adj.ab <- astrolibR::co_aberration(astrolibR::jdcnv(year,1,1,0), star$ra, star$dec, pracma::deg2rad(obliquity(year)))
+    coor$ra <- coor$ra + astrolibR::ten(0,0,adj.ab$d_ra)*sign(adj.ab$d_ra)
+    coor$dec <- coor$dec + astrolibR::ten(0,0,adj.ab$d_dec)*sign(adj.ab$d_dec)
 
     # output
     star$ra <- coor$ra
@@ -260,7 +262,8 @@ palaeo.star = function(star, year = cur.year) {
 #' @param dec Declination of object.
 #' @param loc Locations, either a skyscapeR.object or a vector
 #' containing the latitude and longitude of location, in this order.
-#' @param ...  Any other parameters to be passed unto  \code{\link[astrolibR]{eq2hor}}.
+#' @param res The resolution (in degrees of RA) with which to calculate the path.
+#' @param ...  Any other parameters to be passed unto \code{\link[astrolibR]{eq2hor}}.
 #' @export
 #' @examples
 #' # Visible path of sun on June Solstice on year 3999 BC from London:
@@ -270,7 +273,7 @@ palaeo.star = function(star, year = cur.year) {
 #' loc <- c( london.lat, london.lon )
 #' path <- orbit(sun.dec, loc)
 #' plot(path$az, path$alt, ylim=c(0,90), type='l', xlab='AZ', ylab='ALT', col='red', lwd=2)
-orbit = function(dec, loc, ...) {
+orbit = function(dec, loc, res=0.5, ...) {
   if (class(loc)=='skyscapeR.horizon') {
     lat <- loc$georef[1]
     lon <- loc$georef[2]
@@ -279,7 +282,7 @@ orbit = function(dec, loc, ...) {
     lon <- loc[2]
   }
 
-  ra <- seq(0, 360, by=0.1)
+  ra <- seq(0, 360, by=res)
   aux <- array(NA, c(NROW(ra),2))
 
   for (i in 1:NROW(ra)) {
@@ -315,7 +318,7 @@ eq2horFS = function (ra, dec, jd, lat = 43.0783, lon = -89.865, ws = F,
     if (!missing(b1950)) {
       for (i in 1:length(jd)) {
         tmp = astrolibR::precess(ra[i], dec[i], 1950, j_now[i],
-                      fk4 = TRUE)
+                                 fk4 = TRUE)
         ra[i] = tmp$ra
         dec[i] = tmp$dec
       }
@@ -353,4 +356,130 @@ eq2horFS = function (ra, dec, jd, lat = 43.0783, lon = -89.865, ws = F,
     alt = astrolibR::co_refract(alt, altitude = altitude, ..., to_observed = TRUE)
 
   return(list(alt = alt, az = az, ha = ha))
+}
+
+
+#' Creates a _skyscapeR.object_ for plotting of celestial objects at given epoch
+#'
+#' This function creates an object containing all the necessary information to
+#' plot celestial objects/events unto the many plotting functions of _skyscapeR_
+#' package.
+#' @param name The name(s) of the celestial object(s) or event(s) of interest.
+#' These can be one of the following soli-lunar events: _jS_, _dS_, _eq_, _nmnLX_, _nMjLX_,
+#' _smnLX_, _sMjLX_, or the name of any star in the database. As shorthand, the names
+#' _sun_ and _moon_ can be used to represent all the above solar and lunar events,
+#' respectively. Alternatively, a custom declination value can be used.
+#' @param epoch The year or year range (as an array) one is interested in.
+#' @param col (Optional) The colour for plotting, and differentiating these objects.
+#' Defaults to red for all objects.
+#' @param lty (Optional) Line type (see \code{\link{par}}) used for differentiation.
+#' Only activated for single year epochs.
+#' @param lwd (Optional) Line width (see \code{\link{par}}) used for differentiation.
+#' Only activated for single year epochs.
+#' @export
+#' @examples
+#' # Create a object with solar targets for epoch range 4000-2000 BC:
+#' tt <- object('sun', c(-4000,-2000))
+#'
+#' # Create an object with a few stars for same epoch:
+#' tt <- object(c('Sirius', 'Betelgeuse', 'Antares'), c(-4000,-2000), col=c('white', 'red', 'orange'))
+#'
+#' # Create an object with solstices and a custom declination value:
+#' tt <- object(c('dS','jS', -13), c(-4000,-2000))
+object = function(names, epoch, col = 'red', lty = 1, lwd = 1) {
+  N <- NROW(names)
+
+  if (NROW(col)==1) { col <- rep(col,N) }
+  if (NROW(lty)==1) { lty <- rep(lty,N) }
+  if (NROW(lwd)==1) { lwd <- rep(lwd,N) }
+
+  tt <- c()
+  tt.col <- c()
+  tt.lty <- c()
+  tt.lwd <- c()
+
+  options(warn=-2)
+  for (i in 1:N) {
+    if (is.na(as.numeric(names[i]))) {
+      # sun and moon shorthand
+      if (pracma::strcmp(names[i], 'sun')) {
+        aux <- array(NA, c(NROW(epoch),3))
+        for (j in 1:NROW(epoch)) {
+          aux[j,] <- c(dS(epoch[j]), jS(epoch[j]), eq(epoch[j]))
+        }
+        colnames(aux) <- c('dS','jS','eq')
+        tt <- cbind(tt, aux)
+        tt.col <- c(tt.col, rep(col[i], 3))
+        tt.lty <- c(tt.lty, rep(lty[i], 3))
+        tt.lwd <- c(tt.lwd, rep(lwd[i], 3))
+      }
+
+      if (pracma::strcmp(names[i], 'moon')) {
+        aux <- array(NA, c(NROW(epoch),4))
+        for (j in 1:NROW(epoch)) {
+          aux[j,] <- c(sMjLX(epoch[j]), smnLX(epoch[j]), nmnLX(epoch[j]), nMjLX(epoch[j]))
+        }
+        colnames(aux) <- c('sMjLX', 'smnLX', 'nmnLX', 'nMjLX')
+        tt <- cbind(tt, aux)
+        tt.col <- c(tt.col, rep(col[i], 4))
+        tt.lty <- c(tt.lty, rep(lty[i], 4))
+        tt.lwd <- c(tt.lwd, rep(lwd[i], 4))
+      }
+
+      if (pracma::strcmp(names[i], 'sunandmoon')) {
+        aux <- array(NA, c(NROW(epoch),7))
+        for (j in 1:NROW(epoch)) {
+          aux[j,] <- c(dS(epoch[j]), jS(epoch[j]), eq(epoch[j]), sMjLX(epoch[j]), smnLX(epoch[j]), nmnLX(epoch[j]), nMjLX(epoch[j]))
+        }
+        colnames(aux) <- c('dS','jS','eq','sMjLX', 'smnLX', 'nmnLX', 'nMjLX')
+        tt <- cbind(tt, aux)
+        tt.col <- c(tt.col, rep(col[i], 7))
+        tt.lty <- c(tt.lty, rep(lty[i], 7))
+        tt.lwd <- c(tt.lwd, rep(lwd[i], 7))
+      }
+
+      ####### TO DO ::: include individual solar and lunar targets
+
+      # stars  :::: NEEDS CHANGING TO OUTPUT MIN AND MAX DEC
+      if (sum(sapply(as.character(stars$NAME), pracma::strcmp, s2=names[i]))) {
+        aux <- array(NA, c(NROW(epoch),1))
+        for (j in 1:NROW(epoch)) {
+          aux[j,] <- palaeo.star(do.call(star, list(names[i])), epoch[j])$dec
+        }
+        colnames(aux) <- names[i]
+        tt <- cbind(tt, aux)
+        tt.col <- c(tt.col, col[i])
+        tt.lty <- c(tt.lty, lty[i])
+        tt.lwd <- c(tt.lwd, lwd[i])
+      }
+    }
+
+    # custom dec
+    if (!is.na(as.numeric(names[i]))) {
+      aux <- array(NA, c(NROW(epoch),1))
+      aux[,1] <- rep(as.numeric(names[i]),NROW(epoch))
+      colnames(aux) <- paste0('Custom Dec:',names[i])
+      tt <- cbind(tt, aux)
+      tt.col <- c(tt.col, col[i])
+      tt.lty <- c(tt.lty, lty[i])
+      tt.lwd <- c(tt.lwd, lwd[i])
+    }
+
+  }
+  options(warn=0)
+  rownames(tt) <- epoch
+
+
+  # return result
+  object <- c()
+  object$n <- NCOL(tt)
+  object$decs <- tt
+  object$epoch <- epoch
+  object$col <- tt.col
+  if (NROW(epoch)==1) {
+    object$lty <- tt.lty
+    object$lwd <- tt.lwd
+  }
+  class(object) <- "skyscapeR.object"
+  return(object)
 }
--- R/plotting.R
+++ R/plotting.R
@@ -5,15 +5,24 @@ devtools::use_package("plotrix")
 #' This function creates a polar plot of azimuthal data. It is a wrapper for
 #' \code{\link[plotrix]{polar.plot}}
 #' @param az Array of azimuths. Values outside the [0, 360] range will be ignored.
+#' @param objects (Optional) A skyscapeR.object object created with \code{\link{object}}
+#' for displaying the azimuths of clestial objects. Beware that this assumes a single
+#' location (given by parameter loc) and a flat horizon of zero degrees.
+#' @param loc (Optional) This can be either the latitude of the
+#' location, or a skyscapeR.horizon object. Only necessary for plotting potential
+#' celestial targets.
 #' @param ... Any other parameters to be passed unto \code{\link[plotrix]{polar.plot}}
 #' @export
-#' @seealso \code{\link[plotrix]{polar.plot}}
+#' @seealso \code{\link[plotrix]{polar.plot}}, \code{\link{object}}
 #' @examples
 #' # Plot some azimuth data:
 #' az <- c(120, 100, 93, 97, 88, 115, 112, 67)
-#' orplot(az)
-orplot = function(az, ...) {
-  require(plotrix)
+#' plotAz(az)
+#'
+#' # To visualize this data against the common solar and lunar targets:
+#' tt <- object(c('sun','moon'), epoch=-2000, lty=c(2,3))
+#' plotAz(az, tt, loc=c(35,-8))
+plotAz = function(az, objects, loc, ...) {
 
   ind <- which(az > 360 | az < 0)
   if (length(ind) > 0) {
@@ -27,26 +36,63 @@ orplot = function(az, ...) {
   testpos <- c(0,az)
   label.pos <- c(0,45,90,135,180,225,270,315)
   labels <- c("N","","E","","S","","W","")
-  plotrix::polar.plot(testlen,testpos,main="Orientation Plot",lwd=1.2,line.col='black', start=90, clockwise=T, labels= labels, label.pos = label.pos, show.grid.labels= F, ...)
+  par(mar=c(4, 4, 2, 2) + 0.1)
+  plotrix::polar.plot(testlen, testpos, lwd=1.2, line.col='black', start=90, clockwise=T, labels= labels, label.pos = label.pos, show.grid.labels= F, ...)
+  mtext(paste0('skyscapeR ', packageVersion('skyscapeR'),' Fabio Silva (', substr(packageDescription('skyscapeR')$Date,1,4),')'),3, adj=0, cex=0.5)
+
+  # objects
+  if (!missing(objects) & !missing(loc)) {
+    rise <- c(); set <- c()
+    for (i in 1:objects$n) {
+      orb <- orbit(objects$decs[i], loc)
+      forb <- splinefun(orb$az, orb$alt)
+      rise[i] <- uniroot(forb, interval=c(0, 180))$root
+      set[i] <- uniroot(forb, interval=c(180, 360))$root
+    }
+    tt <- c(rise, set)
+    plotrix::polar.plot(c(0.01,rep(1,NROW(tt))), c(0,tt), lwd=c(0,rep(objects$lwd,2)), line.col=c(0,rep(objects$col,2)), lty=c(1,rep(objects$lty,2)), start=90, clockwise=T, add=T)
+    plotrix::radial.plot.labels(c(0.01,rep(1,NROW(rise))), c(0,rise), units='polar', start=pi*90/180, clockwise=T, labels=c('', colnames(objects$decs)), cex=0.6, col=c('black',objects$col), pos=4, offset=0.5)
+    plotrix::radial.plot.labels(c(0.01,rep(1,NROW(set))), c(0,set), units='polar', start=pi*90/180, clockwise=T, labels=c('', colnames(objects$decs)), cex=0.6, col=c('black',objects$col), pos=2, offset=0.5)
+  }
 }
 
 #' Plot a curvigram
 #'
 #' This function creates a plot of a curvigram
 #' @param curv Object of \emph{skyscapeR.curv} format, created using \code{\link{curvigram}}.
+#' @param objects (Optional) A skyscapeR.object object created with \code{\link{object}}
+#' for displaying the dclination of clestial objects.
+#' @param xlim Array of two values restricting the horizontal range of the plot.,
 #' @param ... Any other parameters to be passed unto \code{\link{plot.default}}.
 #' @export
-#' @seealso \code{\link{curvigram}}
+#' @seealso \code{\link{curvigram}}, \code{\link{object}}
 #' @examples
-#' # Plot some azimuth data:
-#' decs <- data(RugglesRSC)
-#' curv <- curvigram(decs, sd)
-#' plot(curv)
-plot.skyscapeR.curv = function(curv, xlim=NULL, ...) {
+#' # Plot the curvigram of Recumbent Stone Circles:
+#' data(RugglesRSC)
+#' curv <- curvigram(RugglesRSC$Dec, sd=2)
+#' plot(curv, xlim=c(-40,0))
+#'
+#' # Redo the plot to include lunar extreme declinations:
+#' LEx <- objects(c('smnLX','sMjLX'), -2000, col='red', lty=2)
+#' plot(curv, LEx, xlim=c(-40,0))
+plot.skyscapeR.curv = function(curv, objects, objects.label=T, xlim=NULL, ...) {
+  par(mar=c(4, 4, 2, 2) + 0.1)
   if (is.null(xlim)) { xlim <- c(min(curv$dec),max(curv$dec)) }
   plot.default(-100,-100, xlab='Declination', ylab='Density', xlim=xlim, ylim=c(0,max(curv$density)), axes=F, ...)
   axis(1); axis(2)
   lines(curv$dec, curv$density, lwd=1.5, col='blue')
+  box()
+  mtext(paste0('skyscapeR ',packageVersion('skyscapeR'),' Fabio Silva (', substr(packageDescription('skyscapeR')$Date,1,4),')'),3, adj=0, cex=0.5)
+
+  # objects
+  if (!missing(objects)) {
+    for (i in 1:objects$n) {
+      abline(v=objects$dec[i], col=objects$col[i], lwd=objects$lwd[i], lty=objects$lty[i])
+      if (objects.label) {
+        text(objects$dec[i], .95*par('usr')[4], colnames(objects$decs)[i], col=objects$col[i], pos=4, offset=0.2, cex=0.7)
+      }
+    }
+  }
 }
 
 
@@ -60,13 +106,20 @@ plot.skyscapeR.curv = function(curv, xlim=NULL, ...) {
 #' @param az0 Leftmost azimuth of plot. Defaults to 0 degrees, i.e. North at the left.
 #' @param zoom Boolean that controls whether to provide a zoomed-in view of 100 degrees in
 #' azimuth and 5 degrees of altitude above the horizonline. Defaults to FALSE.
+#' @param objects (Optional) A skyscapeR.object object created with \code{\link{object}}
+#' for displaying the paths of clestial objects.
 #' @param ... Any other parameters to be passed unto \code{\link{plot.default}}.
 #' @export
-#' @seealso \code{\link{download.HWT}}
+#' @seealso \code{\link{download.HWT}}, \code{\link{object}}
 #' @examples
+#' # Plot a horizon retrieved from HeyWhatsThat:
 #' hor <- download.HWT('HIFVTBGK')
 #' plot(hor)
-plot.skyscapeR.horizon <- function(hor, show.az=F, max.alt, az0 = 0, zoom=F, ...) {
+#'
+#' # Add the paths of the ssolstice sun in the year 1999 BC:
+#' tt <- object(c('jS','dS'), -2000, 'blue')
+#' plot(hor, objects=tt)
+plot.skyscapeR.horizon <- function(hor, show.az=F, max.alt, az0 = 0, zoom=F, objects, ...) {
   # rejiggle so plot starts at given start point
   if (az0 < -360) { az0 <- az0 + 360 }
   ind <- which(hor$az < az0)
@@ -97,11 +150,7 @@ plot.skyscapeR.horizon <- function(hor, show.az=F, max.alt, az0 = 0, zoom=F, ...
   }
 
   par(mar=c(2,1,1,1))
-  plot.default(hor$az, hor$alt, type='l', xlab = xx, ylab = "", yaxs='i', xaxs='i', ylim = yl, xlim = xl, axes=F, lwd=5, ...)
-  x <- c(hor$az, rev(hor$az))
-  y <- c(hor$alt, rep(-20,NROW(hor$az)))
-  polygon(x ,y, col=rgb(217/255,95/255,14/255,1))
-
+  plot(-99999,-99999, xlab = xx, ylab = "", yaxs='i', xaxs='i', ylim = yl, xlim = xl, axes=F, lwd=5, ...)
   if (show.az == T) {
     axis(1, at = at, labels = ll)
   } else {
@@ -110,6 +159,22 @@ plot.skyscapeR.horizon <- function(hor, show.az=F, max.alt, az0 = 0, zoom=F, ...
   }
   # axis(2, at = seq(0,yl[2],by=10))
   box()
+  mtext(paste0('skyscapeR ',packageVersion('skyscapeR'),' Fabio Silva (', substr(packageDescription('skyscapeR')$Date,1,4),')'),3, adj=0, cex=0.5)
+
+  # objects
+  if (!missing(objects)) {
+    for (i in 1:objects$n) {
+      orb <- orbit(objects$dec[i], hor, res=0.5)
+      plot(orb, objects$col[i])
+    }
+  }
+
+  # Horizon line
+  line(hor$az, hor$alt)
+  x <- c(hor$az, rev(hor$az))
+  y <- c(hor$alt, rep(-20,NROW(hor$az)))
+  polygon(x ,y, col=rgb(217/255,95/255,14/255,1))
+
 }
 
 
@@ -118,7 +183,6 @@ plot.skyscapeR.horizon <- function(hor, show.az=F, max.alt, az0 = 0, zoom=F, ...
 #' This function adds the visible path of a celestial object to a horizon plot.
 #' @param orbit Object of \emph{skyscapeR.orbit} format.
 #' @param col String with colour to plot path in. Defaults to red.
-#' @export
 #' @seealso \code{\link{plot.skyscapeR.horizon}}, \code{\link{orbit}}
 #' @examples
 #' hor <- download.HWT('HIFVTBGK')
@@ -126,16 +190,8 @@ plot.skyscapeR.horizon <- function(hor, show.az=F, max.alt, az0 = 0, zoom=F, ...
 #'
 #' path <- orbit(dS(-2500),hor)
 #' plot(path, col='blue')
-plot.skyscapeR.orbit <- function(orbit, col, ...) {
-  if (missing(col)) { col <- "red" }
-
-  options(warn=2)
-  test <- try (par(new=T), silent=T)
-  options(warn=0)
-  if (class(test)=='try-error') {
-    hor <- createHor(c(0,180,360), c(0,0,0), orbit$georef[1], orbit$georef[2], 'Flatland')
-    plot(hor)
-  }
+#' @noRd
+plot.skyscapeR.orbit <- function(orbit, col) {
 
   ind.break <- which(abs(diff(orbit$az)) > 1)
   if (NROW(ind.break) > 0) {
